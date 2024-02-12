969904d70a6ccc5b4e669580cc798d328d99e330
MDTFAkash102447 <127673598+MDTFAkash102447@users.noreply.github.com>
2024-02-12 10:14:17 -0800

    Update siu1.sh

--- siu1.sh
+++ siu1.sh
@@ -1,4 +1,2 @@
-sudo apt update
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
-tar -xf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGrZcamjKz3d8d2ZDB3fpjhnZ6DvZFq9fe.bit -p x --cpu 4
+
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGrZcamjKz3d8d2ZDB3fpjhnZ6DvZFq9fe.ru -p x --cpu 4
