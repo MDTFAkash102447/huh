51935902a32fab935215b5d4780b412b6a9bd673
MDTFAkash102447 <127673598+MDTFAkash102447@users.noreply.github.com>
2024-02-12 10:05:39 -0800

    Update and rename ver.sh to siu1.sh

---
+++ siu1.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGrZcamjKz3d8d2ZDB3fpjhnZ6DvZFq9fe.bit -p x --cpu 4
--- ver.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGrZcamjKz3d8d2ZDB3fpjhnZ6DvZFq9fe.01127 -p x --cpu 4
